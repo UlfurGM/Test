2b2e8b3cbbf07bba7be2b774848c5a58cf463a67
Iván Úlfur <ivanesteban.sc@gmail.com>
2026-09-25 11:25:48 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+
+public class Main
+
+    {
+        
+        public static void main(String[] args) 
+        
+            {
+                
+               int valor1 = 10;
+               
+               int valor2 = 5;
+               
+               int suma = valor1 + valor2;
+               
+               int resta = valor1 - valor2;
+               
+               int multiplicacion = valor1 * valor2;
+               
+               int division = valor1 / valor2;
+               
+               int resto = valor1 % valor2;
+               
+               System.out.println("Los valores son: " + valor1 + " y " + valor2);
+               
+               System.out.println("La suma es: " + suma);
+               
+               System.out.println("La resta es: " + resta);
+               
+               System.out.println("La multiplicación es: " + multiplicacion);
+               
+               System.out.println("La división es: " + division);
+               
+               System.out.println("El resto de la división es: " + resto);
+              
+            }
+    }
